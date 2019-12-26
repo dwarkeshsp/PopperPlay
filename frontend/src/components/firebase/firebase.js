bb09f115502e3ce9fb77c44663c30d6580182ac9
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2019-12-26 09:06:31 -0600

    sign up error

--- frontend/src/components/firebase/firebase.js
+++ frontend/src/components/firebase/firebase.js
@@ -1,23 +1,67 @@
-import app from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
-import config from "./config";
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import app from 'firebase/app';
+
+import config from './config';
 
 class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
-    this.db = app.database();
+    this.db = app.firestore();
   }
   // *** Auth API ***
   doCreateUserWithEmailAndPassword = (email, password) =>
-    this.auth.createUserWithEmailAndPassword(email, password);
+      this.auth.createUserWithEmailAndPassword(email, password);
+
   doSignInWithEmailAndPassword = (email, password) =>
-    this.auth.signInWithEmailAndPassword(email, password);
+      this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignInWithGoogle = () => this.auth.signInWithPopup(this.googleProvider);
+
+  doSignInWithFacebook = () => this.auth.signInWithPopup(this.facebookProvider);
+
+  doSignInWithTwitter = () => this.auth.signInWithPopup(this.twitterProvider);
 
   doSignOut = () => this.auth.signOut();
+
   doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doSendEmailVerification = () => this.auth.currentUser.sendEmailVerification({
+    url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+  });
+
   doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  // *** Merge Auth and DB User API *** //
+
+  onAuthUserListener = (next, fallback) =>
+      this.auth.onAuthStateChanged(authUser => {
+        if (authUser) {
+          this.user(authUser.displayName).get().then(snapshot => {
+            const dbUser = snapshot.data();
+
+            // default empty roles
+            if (!dbUser.roles) {
+              dbUser.roles = {};
+            }
+
+            // merge auth and db user
+            authUser = {
+              uid: authUser.uid,
+              email: authUser.email,
+              emailVerified: authUser.emailVerified,
+              providerData: authUser.providerData,
+              ...dbUser,
+            };
+
+            next(authUser);
+          });
+        } else {
+          fallback();
+        }
+      });
 
   // *** Persistence API ***
 
@@ -28,8 +72,8 @@ class Firebase {
   // *** User API ***
   currentUser = () => this.auth.currentUser;
   UserInfo = () => this.auth.UserInfo;
-  user = uid => this.db.ref(`users/${uid}`);
-  users = () => this.db.ref("users");
+  user = username => this.db.doc(`users/${username}`);
+  users = () => this.db.collection('users');
 }
 
 export default Firebase;
