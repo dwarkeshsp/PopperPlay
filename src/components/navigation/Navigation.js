ed076eced537d59707563379048a043719e98896
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-01-12 01:40:39 -0600

    appbar behaves properly

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,77 +1,23 @@
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Slide from "@material-ui/core/Slide";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import React from "react";
 import { Link } from "react-router-dom";
 import Account from "./Account";
-import Header from "./Header";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import SearchIcon from "@material-ui/icons/Search";
-import HomeIcon from "@material-ui/icons/Home";
-import Box from "@material-ui/core/Box";
-import IconButton from "@material-ui/core/IconButton";
-import Slide from "@material-ui/core/Slide";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  icon: {
-    marginRight: theme.spacing(2)
-  },
+
   title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    },
-    color: theme.palette.primary,
     textDecoration: "none",
     flexGrow: 1
-  },
-  titleBuffer: {
-    flexGrow: 1
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "auto"
-    }
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit"
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: 120,
-      "&:focus": {
-        width: 200
-      }
-    }
   }
 }));
 
@@ -97,14 +43,6 @@ export default function Navigation(props) {
       <HideOnScroll {...props}>
         <AppBar color="primary">
           <Toolbar>
-            {/* <IconButton
-              aria-label="home"
-              color="inherit"
-              component={Link}
-              to="/"
-            >
-              <HomeIcon />
-            </IconButton> */}
             <Typography
               component={Link}
               to="/"
@@ -115,21 +53,6 @@ export default function Navigation(props) {
             >
               PopperPlay
             </Typography>
-            {/* <Header /> */}
-            {/* <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-                inputProps={{ "aria-label": "search" }}
-              />
-            </div> */}
-            <Box m={1}></Box>
             <Account />
           </Toolbar>
         </AppBar>
