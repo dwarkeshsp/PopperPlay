0d98fd26d86cab2e3a5b5ec2092426dd19cb8665
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-01-12 00:17:57 -0600

    add back config

---
+++ frontend/src/components/firebase/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyBUE6zPlUB7ZMhKgTGohFz0hXcVynEeO5E",
+  authDomain: "popper-play.firebaseapp.com",
+  databaseURL: "https://popper-play.firebaseio.com",
+  projectId: "popper-play",
+  storageBucket: "popper-play.appspot.com",
+  messagingSenderId: "225840058733",
+  appId: "1:225840058733:web:7f209ded1f5c18c2928a64"
+};
+
+export default config;
