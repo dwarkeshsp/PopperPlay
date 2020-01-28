e118f5a26f0ba9df94090fed34e1709f922ee546
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-01-28 14:12:05 -0600

    click node to redirect

--- src/components/util/Graph.js
+++ src/components/util/Graph.js
@@ -1,6 +1,6 @@
 import { Graph as D3Graph } from "react-d3-graph";
 import React from "react";
-import { withFirebase } from "../firebase";
+import { useHistory } from "react-router-dom";
 
 // graph payload (with minimalist structure)
 async function itemToGraphData(item, setData, problem) {
@@ -11,13 +11,13 @@ async function itemToGraphData(item, setData, problem) {
       if (doc.exists) {
         const nodeData = doc.data();
         data.nodes.push({
-          id: doc.id,
+          id: type + doc.id,
           name: nodeData.title,
           size: getSize(nodeData.votes)
         });
         data.links.push({
-          source: item.id,
-          target: doc.id
+          source: (problem ? "/problem/" : "/conjecture/") + item.id,
+          target: type + doc.id
         });
         console.log(doc);
       }
@@ -28,13 +28,13 @@ async function itemToGraphData(item, setData, problem) {
       if (doc.exists) {
         const nodeData = doc.data();
         data.nodes.push({
-          id: doc.id,
+          id: type + doc.id,
           name: nodeData.title,
           size: getSize(nodeData.votes)
         });
         data.links.push({
-          source: doc.id,
-          target: item.id
+          source: type + doc.id,
+          target: (problem ? "/problem/" : "/conjecture/") + item.id
         });
       }
     });
@@ -43,7 +43,7 @@ async function itemToGraphData(item, setData, problem) {
   let data = {
     nodes: [
       {
-        id: item.id,
+        id: (problem ? "/problem/" : "/conjecture/") + item.id,
         name: item.title,
         size: 1000
       }
@@ -54,45 +54,65 @@ async function itemToGraphData(item, setData, problem) {
   if (problem) {
     await Promise.all(
       item.childConjectures.map(async node => {
-        await childNode(node, "c");
+        await childNode(node, "/conjecture/");
       })
     );
     await Promise.all(
       item.parentConjectures.map(async node => {
-        await parentNode(node, "c");
+        await parentNode(node, "/conjecture/");
       })
     );
   } else {
     await Promise.all(
       item.childConjectures.map(async node => {
-        await childNode(node, "c");
+        await childNode(node, "/conjecture/");
       })
     );
     await Promise.all(
       item.parentConjectures.map(async node => {
-        await parentNode(node, "c");
+        await parentNode(node, "/conjecture/");
       })
     );
     await Promise.all(
       item.childProblems.map(async node => {
-        await childNode(node, "p");
+        await childNode(node, "/problem/");
       })
     );
     await Promise.all(
       item.parentProblems.map(async node => {
-        await parentNode(node, "p");
+        await parentNode(node, "/problem/");
       })
     );
   }
   setData(data);
 }
 
-// the graph configuration, you only need to pass down properties
-// that you want to override, otherwise default ones will be used
+export default function Graph({ item, problem }) {
+  const history = useHistory();
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    itemToGraphData(item, setData, problem);
+  }, []);
+
+  return (
+    <React.Fragment>
+      {data && data.nodes.length > 1 && (
+        <D3Graph
+          id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+          data={data}
+          config={config}
+          onClickNode={nodeID => history.push(nodeID)}
+        />
+      )}
+    </React.Fragment>
+  );
+}
+
 const config = {
   directed: true,
   automaticRearrangeAfterDropNode: true,
-  collapsible: true,
   height: 400,
   highlightDegree: 2,
   highlightOpacity: 0.2,
@@ -140,30 +160,3 @@ const config = {
     type: "STRAIGHT"
   }
 };
-
-function Graph({ item, problem, firebase }) {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    itemToGraphData(item, setData, problem, firebase);
-  }, []);
-
-  const onClickNode = function(nodeId) {
-    window.alert(`Clicked node ${nodeId}`);
-  };
-
-  return (
-    <React.Fragment>
-      {data && data.nodes.length > 1 && (
-        <D3Graph
-          id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
-          data={data}
-          config={config}
-          onClickNode={onClickNode}
-        />
-      )}
-    </React.Fragment>
-  );
-}
-
-export default withFirebase(Graph);
