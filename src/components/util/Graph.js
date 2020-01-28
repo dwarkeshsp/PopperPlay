ef77dfb1d0d9f6a69fc6ec59fe2b0bce02eee2df
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-01-27 21:07:58 -0600

    tentative graph component

--- src/components/util/Graph.js
+++ src/components/util/Graph.js
@@ -5,71 +5,139 @@ import { withFirebase } from "../firebase";
 // graph payload (with minimalist structure)
 async function itemToGraphData(item, setData, problem, firebase) {
   const getSize = votes => 50 * votes + 500;
-  function problemData() {
-    item.childConjectures.map(conjecture => {
-      console.log("before");
-      conjecture.get().then(doc => {
-        if (doc.exists) {
-          const content = doc.data();
-          data.nodes.push({
-            id: content.title,
-            size: getSize(content.votes)
-          });
-          data.links.push({
-            source: item.title,
-            target: content.title
-          });
-        }
-      });
-    });
-  }
 
+  const childNode = node =>
+    node.get().then(doc => {
+      if (doc.exists) {
+        const nodeData = doc.data();
+        data.nodes.push({
+          id: nodeData.title,
+          size: getSize(nodeData.votes)
+        });
+        data.links.push({
+          source: item.title,
+          target: nodeData.title
+        });
+      }
+    });
+
+  const parentNode = node =>
+    node.get().then(doc => {
+      if (doc.exists) {
+        const nodeData = doc.data();
+        data.nodes.push({
+          id: nodeData.title,
+          size: getSize(nodeData.votes)
+        });
+        data.links.push({
+          source: nodeData.title,
+          target: item.title
+        });
+      }
+    });
+
+  // main node
   let data = {
-    nodes: [{ id: item.title, size: 500 }],
+    nodes: [{ id: item.title, size: 1000 }],
     links: []
   };
-  await problemData();
+
+  if (problem) {
+    await Promise.all(
+      item.childConjectures.map(async node => {
+        await childNode(node);
+      })
+    );
+    await Promise.all(
+      item.parentConjectures.map(async node => {
+        await parentNode(node);
+      })
+    );
+  } else {
+    await Promise.all(
+      item.childConjectures.map(async node => {
+        await childNode(node);
+      })
+    );
+    await Promise.all(
+      item.parentConjectures.map(async node => {
+        await parentNode(node);
+      })
+    );
+    await Promise.all(
+      item.childProblems.map(async node => {
+        await childNode(node);
+      })
+    );
+    await Promise.all(
+      item.parentProblems.map(async node => {
+        await parentNode(node);
+      })
+    );
+  }
   setData(data);
-  //   return {
-  //     nodes: [
-  //       {
-  //         id: item.title,
-
-  //         size: 10 * 100 + 200
-  //       },
-  //       { id: "Harry" },
-  //       { id: "Sally" },
-  //       { id: "Alice" }
-  //     ],
-  //     links: [
-  //       { source: item.title, target: "Alice" },
-
-  //       { source: item.title, target: "Sally" },
-  //       { source: item.title, target: "Harry" }
-  //     ]
-  //   };
 }
 
 // the graph configuration, you only need to pass down properties
 // that you want to override, otherwise default ones will be used
-const myConfig = {
-  nodeHighlightBehavior: true,
+const config = {
   directed: true,
+  automaticRearrangeAfterDropNode: true,
+  collapsible: true,
+  height: 400,
+  highlightDegree: 2,
+  highlightOpacity: 0.2,
+  linkHighlightBehavior: true,
+  maxZoom: 12,
+  minZoom: 0.05,
+  nodeHighlightBehavior: true,
+  panAndZoom: false,
+  staticGraph: false,
+  width: 800,
+  d3: {
+    alphaTarget: 0.05,
+    gravity: -250,
+    linkLength: 120,
+    linkStrength: 2
+  },
   node: {
-    color: "lightgreen",
-    size: 120,
-    highlightStrokeColor: "blue",
-    mouseCursor: "pointer"
+    color: "#d3d3d3",
+    fontColor: "black",
+    fontSize: 10,
+    fontWeight: "normal",
+    highlightColor: "red",
+    highlightFontSize: 14,
+    highlightFontWeight: "bold",
+    highlightStrokeColor: "red",
+    highlightStrokeWidth: 1.5,
+    labelProperty: n => (n.name ? `${n.id} - ${n.name}` : n.id),
+    mouseCursor: "crosshair",
+    opacity: 0.9,
+    renderLabel: true,
+    size: 200,
+    strokeColor: "none",
+    strokeWidth: 1.5,
+    svg: "",
+    symbolType: "circle",
+    viewGenerator: null
   },
   link: {
-    highlightColor: "lightblue"
+    color: "lightgray",
+    highlightColor: "red",
+    mouseCursor: "pointer",
+    opacity: 1,
+    semanticStrokeWidth: true,
+    strokeWidth: 3,
+    type: "STRAIGHT"
   }
 };
 
 function Graph({ item, problem, firebase }) {
   const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {}, []);
+  React.useEffect(() => {
+    itemToGraphData(item, setData, problem, firebase);
+  }, []);
 
   return (
     <React.Fragment>
@@ -77,7 +145,7 @@ function Graph({ item, problem, firebase }) {
         <D3Graph
           id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
           data={data}
-          config={myConfig}
+          config={config}
         />
       )}
     </React.Fragment>
