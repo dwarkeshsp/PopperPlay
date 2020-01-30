657224c92918ba33e82779861535ccc781f3a58a
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2020-01-29 20:11:34 -0600

    added original config

--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -1,4 +1,15 @@
 const config = {
+  apiKey: "AIzaSyBUE6zPlUB7ZMhKgTGohFz0hXcVynEeO5E",
+  authDomain: "popper-play.firebaseapp.com",
+  databaseURL: "https://popper-play.firebaseio.com",
+  projectId: "popper-play",
+  storageBucket: "popper-play.appspot.com",
+  messagingSenderId: "225840058733",
+  appId: "1:225840058733:web:7f209ded1f5c18c2928a64",
+  measurementId: "G-7H18ZCSCW2"
+};
+
+const testConfig = {
   apiKey: "AIzaSyDIUPIlbClknbcSFMnO02PR8E1cuo_crR8",
   authDomain: "popperplay0.firebaseapp.com",
   databaseURL: "https://popperplay0.firebaseio.com",
@@ -9,4 +20,4 @@ const config = {
   measurementId: "G-9SSX21EFEC"
 };
 
-export default config;
+export { config, testConfig };
